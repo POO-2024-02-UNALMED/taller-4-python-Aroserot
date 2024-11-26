44c956c53237109708537faa25cd61ce524d1150
Aroserot <aroserot@unal.edu.co>
2024-11-26 10:46:48 -0500

    C1

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon="remoto"):##Necesario para que se imprima ese valor por defecto
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):##Hace de toString
+        return f"{self._nombre} {self._salon}"
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,23 +1,27 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes= []): ##Al final solo parametros con valores establecidos
         self._grupo = grupo
+        if asignaturas is None:##En caso de que sea None
+            asignaturas = []
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):##Faltaban los **
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista is None:##En caso de que sea None
+            lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):##"hace de toString"
+        return f"Grupo de estudiantes: {self._grupo}"
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
